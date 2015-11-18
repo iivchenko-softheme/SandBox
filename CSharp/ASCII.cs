48cb20f6aeb8a633546afe82ba09fdfc1fd7902a
shogun <iivchenko@live.com>
2015-11-18 12:26:35 +0000

    Add ASCII

---
+++ CSharp/ASCII.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using NUnit.Framework;
+
+namespace CSharp
+{
+    [TestFixture]
+    public class ASCII
+    {
+        [Test]
+        public void SymToCode()
+        {
+            var syms = new[] { 'シ', 'À', '!', '·' };
+
+            foreach (var sym in syms)
+            {
+                Console.WriteLine(sym + " " + (int)sym);
+            }
+        }
+    }
+}
---
+++ CSharp/TypeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CSharp
+{
+    [TestFixture]
+    public sealed class TypeTests
+    {
+        [Test]
+        public void ToArrayTest()
+        {
+            var list = new List<int> { 1, 2 };
+
+            foreach (var element in list.Select(x => x.GetType()).ToArray())
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
